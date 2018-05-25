bae879f4e01639f5e6a82ced387c96cbff613076
Ryan Northey <ryan@synca.io>
2018-05-25 22:49:11 +0100

    Add daemon-reload implementation

--- ctrl/systemd/sysctl.py
+++ ctrl/systemd/sysctl.py
@@ -8,6 +8,26 @@ from .interfaces import ISysctl
 @interface.implementer(ISysctl)
 class SystemdSysctl(object):
 
+    async def await_send_reply(self, method, *args):
+        conn = await self.get_connection()
+        message = dbussy.Message.new_method_call(
+            destination='org.freedesktop.systemd1',
+            path='/org/freedesktop/systemd1',
+            iface='org.freedesktop.systemd1.Manager',
+            method=method)
+        message.append_objects(*args)
+        return await conn.send_await_reply(message)
+
+    async def get_connection(self):
+        return await dbussy.Connection.bus_get_async(
+            dbussy.DBUS.BUS_SYSTEM, private=False)
+
+    async def daemon_reload(self):
+        print("Reloading daemons")
+        # busctl call org.freedesktop.systemd1 /org/freedesktop/systemd1
+        # org.freedesktop.systemd1.Manager Reload
+        return await self.send_await_reply('Reload')
+
     async def start(self, service):
         pass
 
@@ -18,18 +38,11 @@ class SystemdSysctl(object):
         # busctl call org.freedesktop.systemd1 /org/freedesktop/systemd1
         # org.freedesktop.systemd1.Manager
         # StopUnit ss "controller-translate.service" "replace"
-        conn = await dbussy.Connection.bus_get_async(
-            dbussy.DBUS.BUS_SYSTEM, private=False)
-        message = dbussy.Message.new_method_call(
-            destination='org.freedesktop.systemd1',
-            path='/org/freedesktop/systemd1',
-            iface='org.freedesktop.systemd1.Manager',
-            method='StopUnit')
-        message.append_objects(
+        reply = await self.send_await_reply(
+            'StopUnit',
             'ss',
             'controller-%s.service' % service,
             'replace')
-        reply = await conn.send_await_reply(message)
         print(repr(reply.all_objects))
         return 'Service (%s) stopped' % service
 
